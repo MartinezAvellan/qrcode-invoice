ce7c54c7c1552c3c80aaa87e2ad4acca66f02e99
Hugo A Martinez <martinez.avellan@hotmail.com>
2022-07-02 21:43:32 +0100

    QT-3

--- handler.py
+++ handler.py
@@ -5,15 +5,13 @@ new_token = NewToken()
 
 
 def handler(event, context):
-    print(event)
-    print(context)
     request_id = get_request_id(event, context)
     message = is_str_or_dict(event)
     try:
         body = is_str_or_dict(message['Records'][0]['body'])
         token, emissor = new_token.get_token(request_id, body)
-
-
+        print(token)
+        print(emissor)
     except Exception as e:
         print(e)
         raise
@@ -21,40 +19,39 @@ def handler(event, context):
 
 if __name__ == '__main__':
     eeeee = {'Records': [
-        {'messageId': 'c03477e0-ca4e-4aa6-9a4e-92fde65338ed',
-         'receiptHandle': 'AQEBwKqicQcoNIBxOvSZP0gPpDhStbEBQ==',
-         'body': '{"file_name": "EcosyInstdePagamentoSA29042022.txt", '
-                 '"bucket_name": "read-invoice", '
-                 '"nome_cedente": "Banco EC S.A.", '
-                 '"cnpj_cedente": "33264668000103", '
-                 '"data_geracao": "29042022", '
-                 '"nome_cliente": "FABILANIA SANDES", '
-                 '"cpf_cnpj": "99958773414", '
-                 '"conta": "682680", '
-                 '"cep": "71505235", '
-                 '"cidade": "Barueri", '
-                 '"estado": "DF", '
-                 '"endereco": "Av. Tambore", '
-                 '"numero": "267", "complemento": "Complemento", '
-                 '"valor_fatura": "00000000204655", '
-                 '"idtx": "QT332646680001032904202200000000001"}',
-         'attributes': {
-             'ApproximateReceiveCount': '1',
-             'SentTimestamp': '1656769309277',
-             'SenderId': 'AIDAYT3YFZIESZ6X7UGX6',
-             'ApproximateFirstReceiveTimestamp': '1656769309287'
-         }, 'messageAttributes': {
-            'request_id': {
-                'stringValue': 'b2afa9b2-fa0c-11ec-b2b5-50e0853f09c7',
-                'stringListValues': [],
-                'binaryListValues': [],
-                'dataType': 'String'}},
-         'md5OfMessageAttributes': 'ce883ec81ef758f8dab223045c034f4d',
-         'md5OfBody': '4603b0f2f1c3bda4cc035441cb2b5249',
-         'eventSource':
-             'aws:sqs',
-         'eventSourceARN': 'arn:aws:sqs:sa-east-1:592420653577:loc-invoice',
+        {'messageId': '154e99d1-f79c-4f23-8d2d-169ecc9e4bbc',
+         'receiptHandle': 'AQEB+f2dex7L5+dkYg==',
+         'body': '{\n   "file_name":"EcosyInstdePagamentoSA29042022.txt",\n   '
+                 '"bucket_name":"read-invoice",\n   '
+                 '"nome_cedente":"Banco EC S.A.",\n   '
+                 '"cnpj_cedente":"33264668000103",\n   '
+                 '"data_geracao":"29042022",\n   '
+                 '"nome_cliente":"FABILANIA SANDES",\n   '
+                 '"cpf_cnpj":"99958773414",\n   '
+                 '"conta":"682680",\n   '
+                 '"cep":"71505235",\n   '
+                 '"cidade":"Barueri",\n   '
+                 '"estado":"DF",\n   '
+                 '"endereco":"Av. Tambore",\n   '
+                 '"numero":"267",\n   '
+                 '"complemento":"Complemento",\n   '
+                 '"valor_fatura":"00000000204655",\n   '
+                 '"idtx":"QT332646680001032904202200000000001",\n   '
+                 '"loc":{\n      "url":"api-h.developer.btgpactual.com/v1/p/v2/bd2f1763b529490c97792f543fd1d01c",\n      '
+                 '"emv":"00020101021226930014br.gov.bcb.pix2571api-h.developer.btgpactual.com/v1/p/v2/bd2f1763b529490c97792f543fd1d01c5204010053039865802BR5923Joselito de Souza Silva6007Barueri61087150523562070503***630471FB",\n      '
+                 '"locType":"COB",\n      '
+                 '"id":"4078206097511746876",\n      '
+                 '"idTx":"RQT332646680001032904202200000000019999",\n      '
+                 '"loc":"bd2f1763-b529-490c-9779-2f543fd1d01c"\n   }'
+                 '\n}', 'attributes': {'ApproximateReceiveCount': '1',
+                                       'SentTimestamp': '1656794186254',
+                                       'SenderId': 'AIDAYT3YFZIESZ6X7UGX6',
+                                       'ApproximateFirstReceiveTimestamp': '1656794186260'
+                                       }, 'messageAttributes': {}, 'md5OfBody': '9169a31dbb0d4efce4eeb484e9e725e2',
+         'eventSource': 'aws:sqs',
+         'eventSourceARN': 'arn:aws:sqs:sa-east-1:592420653577:qrcode-invoice',
          'awsRegion': 'sa-east-1'}
     ]}
+
 
     handler(eeeee, None)
